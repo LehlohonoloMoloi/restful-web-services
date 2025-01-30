fad6faf75dad479b8799504cca5b08d78819bf8e
Lehlohonolo Moloi <lehlohonolo@bbd.co.za>
2025-01-30 21:28:58 +0200

    Implemented generic exception handling for all resources.

---
+++ src/main/java/com/zen/rest/webservices/restful_web_services/exception/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.zen.rest.webservices.restful_web_services.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
